15601ab833b79f7803f74ddd9b5459e30c90bddd
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-06-13 18:16:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,17 @@
 def my_collect(array)
+<<<<<<< HEAD
   i = 0 
   collect = []
   while i < array.length 
     collect << yield(array[i])
     i = i + 1 
+=======
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+>>>>>>> 79053d3d699408184ca235323df7231dfa769c92
   end
   collect
 end
